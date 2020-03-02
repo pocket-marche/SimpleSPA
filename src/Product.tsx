be7731a9622cc302dc8b1d46d532d95d62d9fd38
wmoai <wmoaism@gmail.com>
2020-03-02 13:51:52 +0900

    add link

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 
 export function Product() {
   const {id} = useParams<{id: string}>()
-  return <div>This is SPA Product {id}</div>
+  return (
+    <div>
+      <h1>This is SPA Product {id}</h1>
+      <div>
+        <Link to={`/products/${Number(id) + 1}`}>Next with Link</Link>
+      </div>
+      <div>
+        <a href={`/products/${Number(id) + 1}`}>Next with a tag</a>
+      </div>
+    </div>
+  )
 }
